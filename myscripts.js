0cb6cb71554c882aaa5f461ff58ec7b64804506c
a-okeeffe <aokeeffe@terpmail.umd.edu>
2025-04-11 23:45:52 -0400

    added basic site navigation (menu bar, js)

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+/* opens the navigation bar */
+function openNav(){
+    document.getElementById("menu_bar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+/* closes the navigation bar*/
+function closeNav(){
+    document.getElementById("menu_bar").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+}
